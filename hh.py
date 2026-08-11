3be9de09bfec282412cadd910832a02583c24994
Abin-ms <abinn.mss@gmail.com>
2026-08-11 11:28:33 +0530

    ahaj

---
+++ hh.py
@@ -0,0 +1,32 @@
+# print("haha")
+# print(3.14)
+# print(300)
+# a=10
+# print(a)
+# b=20
+# print(b)
+
+# a = 10
+# print(a)
+# a = 15
+# print(a)
+# b = "huuuuu"
+# print(b)
+# print(type(b))
+# b = 22
+# print(b)
+# print(type(b))
+
+# b = "hshshs"
+# print(b)
+
+a = 10
+a,b=20,30
+a,b,c = 20,10,30
+print(b)
+
+print(c)
+
+# d,b=20
+a=b=10
+print(b,a)
