583b4f7d728e6d305b992e696751eb1dd319e78b
Abin-ms <abinn.mss@gmail.com>
2026-08-12 14:57:48 +0530

    ahahda

---
+++ day1/d1.py
@@ -0,0 +1,22 @@
+a = 10
+print(type(a))
+
+# identify the data type of each value  25 , 225.0 , "25"  , true , 5 + 2j
+
+b = 25
+c = 225.0
+d = "25"
+e = True
+f = 5 + 2j 
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+
+# <class 'int'>
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+# <class 'bool'>
+# <class 'complex'>
